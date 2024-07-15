cbcf3bc3b02fde6bf51c9b4fb3aa7133e09e2801
fatimamaatouk <100756982+fatimamaatouk@users.noreply.github.com>
2024-07-15 12:41:55 +0300

    test.py

--- test.py
+++ test.py
@@ -1,22 +1,27 @@
 from saving_plans import calculate_savings_plan, update_savings_plan, display_savings_plan
 import streamlit as st
-import json
 
 def main():
     st.title("Savings Plan Calculator")
     income = st.number_input("Enter your monthly income (in $):", min_value=0, step=1000)
     savings_percentage = st.slider("Enter the percentage of income to save(%):", min_value=0, max_value=100, step=1)
     
-    st.write("Enter your monthly expenses (in $):")
+    st.write("Enter your monthly expenses (in $) and Set flexibility scores for each category (Higher Scores means More Flexible):")
     expenses = {}
+    flexibility_scores = {}
+    
     categories = ["rent", "groceries", "dining_out", "entertainment", "utilities", "other"]
     for category in categories:
-        expenses[category] = st.number_input(f"Amount for {category}:", min_value=0, step=50)
+        col1,col2 = st.columns(2)
+        with col1:
+            expenses[category] = st.number_input(f"Amount for {category}:", min_value=0, step=50)
+        with col2:
+            flexibility_scores[category] = st.slider(f"Flexibility Score for {category}:", min_value=0.0, step=0.1, max_value=1.0,value=0.5)
     
     excluded_categories = st.multiselect("Calculate categories to exclude from savings:", options=categories)
     
     if st.button("Calculate Savings Plan"):
-        savings_plan = calculate_savings_plan(expenses, income, savings_percentage)
+        savings_plan = calculate_savings_plan(expenses, income, savings_percentage, flexibility_scores)
         st.subheader("Initial Savings Plan($): ")
         display_savings_plan(savings_plan)
         
